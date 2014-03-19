78d220e846bdeaa97f859e541c2d02beae918cdf
Mercandj <modjow.jm9@gmail.com>
2014-03-19 16:27:16 +0100

    Add.php : Vérification de l'ext du fichier upload

--- php/add.php
+++ php/add.php
@@ -14,7 +14,6 @@ include 'class/Image.php';
     ecrire_fichier($filename, $somecontent);
   }
 
-
   $maxwidth = 200000;
   $maxheight = 200000;
 
@@ -22,50 +21,60 @@ include 'class/Image.php';
   if ($_FILES['image']['size'] > 1048576) $erreur = "Le fichier est trop gros";
   $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
   $extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')  ,1)  );
-  if ( in_array($extension_upload,$extensions_valides) ) echo "Extension correcte";
-  $image_sizes = getimagesize($_FILES['image']['tmp_name']);
-  if ($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight) $erreur = "Image trop grande";
 
-  
-  if(!isset($_SESSION))
-    session_start();
-  
+  // Vérification de l'extension
+  if ( in_array($extension_upload,$extensions_valides) ) {
+    $image_sizes = getimagesize($_FILES['image']['tmp_name']);
 
-  //Créer un dossier
-  @mkdir('./../bdd_images/'.$_SESSION['user'].'/', 0777, true);
- 
-  //Créer un identifiant difficile à deviner
-  $nom = md5(uniqid(rand(), true));
+    // Vérification de la taille de l'image
+    if ( !($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight) ) {
 
-  $nom = "./../bdd_images/".$_SESSION['user'].'/'.$_FILES['image']['name']/*.".{$extension_upload}"*/;
-  $resultat = move_uploaded_file($_FILES['image']['tmp_name'],$nom);
-  if ($resultat) echo "Transfert réussi";
+      if(!isset($_SESSION))
+        session_start();
+      
+      // Créer un dossier
+      @mkdir('./../bdd_images/'.$_SESSION['user'].'/', 0777, true);
+     
+      // Créer un identifiant difficile à deviner
+      $nom = md5(uniqid(rand(), true));
+
+      $nom = "./../bdd_images/".$_SESSION['user'].'/'.$_FILES['image']['name']/*.".{$extension_upload}"*/;
+      $resultat = move_uploaded_file($_FILES['image']['tmp_name'],$nom);
+      if ($resultat) echo "Transfert réussi";
 
 
-  $date = date("Y-d-m");
-  $titre = $_POST['titre'];
-  $url = "./../../bdd_images/".$_SESSION['user'].'/'.$_FILES['image']['name']/*.".{$extension_upload}"*/;//$_POST['url'];
-  $description = $_POST['description'];
+      $date = date("Y-d-m");
+      $titre = $_POST['titre'];
+      $url = "./../../bdd_images/".$_SESSION['user'].'/'.$_FILES['image']['name']/*.".{$extension_upload}"*/;//$_POST['url'];
+      $description = $_POST['description'];
 
-  $note = 0;
-  
-  // Connexion à la base de données
-  try {
-    $bdd = new PDO('mysql:host=localhost;dbname=mydb', 'root', '');
+      $note = 0;
+      
+      // Connexion à la base de données
+      try {
+        $bdd = new PDO('mysql:host=localhost;dbname=mydb', 'root', '');
+      }
+      catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
+      }
+
+      // Création d'une image
+      $im = new Image($url, $description, $titre, $url, $_SESSION['user']);
+
+      // INSERT de l'image dans la base de données
+      $req = $bdd->prepare($im->getinsert());
+      $req->execute($im->getarray());
+
+      // Ecriture de logs
+      w_log($titre, $url, $description);
+
+      echo 'L\'image a bien été ajoutée !';
+    }
+    else {
+      echo "Image trop grande";
+    }
+  } 
+  else {
+     echo "Extension incorrecte";
   }
-  catch(Exception $e) {
-    die('Erreur : '.$e->getMessage());
-  }
-
-  // Création d'une image
-  $im = new Image($url, $description, $titre, $url, $_SESSION['user']);
-
-  // INSERT de l'image dans la base de données
-  $req = $bdd->prepare($im->getinsert());
-  $req->execute($im->getarray());
-
-  // Ecriture de logs
-  w_log($titre, $url, $description);
-
-  echo 'L\'image a bien été ajoutée !';
 ?>
