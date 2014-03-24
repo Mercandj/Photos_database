b49f40bad7edf3f729aa5643ec863a75557df12a
andymartel <martelandy764@gmail.com>
2014-03-24 13:58:27 +0100

    Update ImageControleur.php

--- php/controleur/ImageControleur.php
+++ php/controleur/ImageControleur.php
@@ -1,11 +1,13 @@
-
 <?php 
 class ImageControleur {
 
 	public __constructor() {
-
-
+		ImageModele $im = new ImageModele();
+		ImageVue $iv = new ImageVue($im->getImageListe());
+		echo $iv->genererHTML();
 	}
+	
+	
 }
 
 ?>
